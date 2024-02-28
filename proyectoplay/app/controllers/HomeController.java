b0ffff5b7a1b5e902b536476a4fadb96eabcf4db
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-02-28 13:06:49 -0500

    Hello World with play

---
+++ proyectoplay/app/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.mvc.*;
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        String message = "World"; // Define el mensaje que quieres pasar a la vista
+        return ok(index.render(message)); // Pasa el mensaje como argumento a la vista
+    }
+
+}
